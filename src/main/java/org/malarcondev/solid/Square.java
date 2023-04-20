825496954abf088ec59ac07bdb0278ea18df5dfc
Marcos Alarcon <proveedormarcos.alarcon@sura.pe>
2023-04-19 21:01:58 -0500

    first commit

---
+++ src/main/java/org/malarcondev/solid/Square.java
@@ -0,0 +1,13 @@
+package org.malarcondev.solid;
+
+public class Square {
+    private final int lenght;
+
+    public Square(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+}
